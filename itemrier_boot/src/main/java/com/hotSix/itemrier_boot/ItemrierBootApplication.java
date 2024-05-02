0475174bad014f596d0078408e7b6b2d97ce3f0e
kejjin0 <lala0830@naver.com>
2024-05-03 01:16:53 +0900

    first commit

---
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/ItemrierBootApplication.java
@@ -0,0 +1,13 @@
+package com.hotSix.itemrier_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemrierBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemrierBootApplication.class, args);
+	}
+
+}
