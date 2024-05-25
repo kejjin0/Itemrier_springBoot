b312bfab40cf76cedf50c256cf909a6c314a06eb
eunsu14 <7266706@naver.com>
2024-05-25 23:02:41 +0900

    update: 중고거래 entity

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/item/UsedGoods.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/item/UsedGoods.java
@@ -3,45 +3,64 @@ package com.hotSix.itemrier_boot.domain.item;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import com.hotSix.itemrier_boot.domain.user.UserEntity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Getter @ToString
-public class UsedGoods extends ItemInfo{
+@Getter 
+@ToString
+@Table(name = "USEDGOODS")
+public class UsedGoods {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int itemId; // 상품 고유 번호
-    private String condition; // 상품 상태
-    private String contactType; // 연락수단
-    private Date registerDate; // 등록날짜
-    @Column(name = "usedId")
-    private int sellerId; // 글 등록자 아이디
-    @Column(name = "usedId2")
-    private int buyerId; // 구매자 아이디
-    private LocalDateTime transactionDate;	// 거래 날짜
-    private int catId; // 카테고리 아이디
     
-    public UsedGoods() {}
+    private String itemName; // 상품명(제목)
+    
+    private String description; // 상품 설명
+    
+    private String condition; // 상품 상태
+    
+    private String contactType; // 연락수단
+    
+    private String status; // 거래 현황 
+    
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private UserEntity seller; // 판매자
+    
+    @ManyToOne
+    @JoinColumn(name = "catId")
+    private Category category; // 카테고리
+    
+    @ManyToOne
+    @JoinColumn(name = "userId2")    
+    private UserEntity buyer; // 구매자
+    
+    private LocalDateTime registerDate; // 등록날짜
+    
+    private LocalDateTime transactionDate;	// 거래 날짜
+    
+    private int price; // 가격
 
     @Builder
-    public UsedGoods(int itemId, String condition, String contactType, Date registerDate, int sellerId, int buyerId,
-            int catId) {
-        super();
-        this.itemId = itemId;
-        this.condition = condition;
-        this.contactType = contactType;
-        this.registerDate = registerDate;
-        this.sellerId = sellerId;
-        this.buyerId = buyerId;
-        this.catId = catId;
-    }
+	public UsedGoods(int itemId, String itemName, String description, String condition, String contactType,
+			String status, UserEntity seller, Category category, int buyer, LocalDateTime registerDate,
+			LocalDateTime transactionDate, int price) {
+		super();
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.description = description;
+		this.condition = condition;
+		this.contactType = contactType;
+		this.status = status;
+		this.seller = seller;
+		this.category = category;
+		this.buyer = buyer;
+		this.registerDate = registerDate;
+		this.transactionDate = transactionDate;
+		this.price = price;
+	}	
 }
