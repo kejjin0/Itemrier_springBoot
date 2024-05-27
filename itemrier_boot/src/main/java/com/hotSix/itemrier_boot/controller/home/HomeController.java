e3f236a90dba525d6c3bb22175d5255c9b548c48
suhyun <sunny3872@naver.com>
2024-05-28 02:38:38 +0900

    FEAT homepage

---
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/controller/home/HomeController.java
@@ -0,0 +1,26 @@
+//로그인 전, 후 홈페이지 
+package com.hotSix.itemrier_boot.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !(auth.getPrincipal() instanceof String)) {
+            return "redirect:/home";
+        }
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String userHome(Model model) {
+        return "home";
+    }
+}
