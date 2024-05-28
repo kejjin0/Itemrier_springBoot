b890f52536d2e447e1bde9d32b3acf8206b79af0
eunsu14 <7266706@naver.com>
2024-05-28 20:09:10 +0900

    update: ItemInfo 수정

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/item/ItemInfo.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/item/ItemInfo.java
@@ -30,14 +30,14 @@ public abstract class ItemInfo {
 	private String status; // 거래 현황(판매 중, 판매 완료)
 	
 	@ManyToOne
-   @JoinColumn(name="user_Id", nullable = false)
+    @JoinColumn(name="user_Id", nullable = false)
 	private UserEntity seller; // 판매자
 	
 	@ManyToOne
-   @JoinColumn(name="user_Id", nullable = false)
+    @JoinColumn(name="user_Id")
 	private UserEntity buyer; 
 
 	@ManyToOne
-  @JoinColumn(name = "catId", nullable = false)
+    @JoinColumn(name = "catId", nullable = false)
 	private Category category; // 카테고리
 }
