a299a607650bb9c480002900912a100ad8ba9a9f
kejjin0 <lala0830@naver.com>
2024-06-02 03:59:06 +0900

    deliveryInfo domain에 getter setter 추가

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/order/DeliveryInfo.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/domain/order/DeliveryInfo.java
@@ -3,8 +3,12 @@ package com.hotSix.itemrier_boot.domain.order;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter @Setter @ToString
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
