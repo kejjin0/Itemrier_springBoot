e12854e118cd7d8e5c0e5d8710bf723b2253ecd0
minsun <pitapatsun@gmail.com>
2024-06-16 16:36:23 +0900

    feat: 메인화면-로그인화면 연결

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/controller/user/UserController.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/controller/user/UserController.java
@@ -3,10 +3,14 @@ package com.hotSix.itemrier_boot.controller.user;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hotSix.itemrier_boot.domain.user.UserEntity;
 import com.hotSix.itemrier_boot.dto.user.UserDto;
@@ -15,23 +19,33 @@ import com.hotSix.itemrier_boot.service.user.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping(value = "/user")
 public class UserController {
 
 	private final UserService userService;
 	private final UserRepository userRepository;
 	//private final BCryptPasswordEncoder encoder;
 	
+	@GetMapping("/user/login/form") // 로그인
+	public String login() {
+		return "join/login";
+	}
 	
-	@PostMapping("/register") 
+	@GetMapping("/user/register/form")
+	public String resisterUser() {
+		return "join/joinForm";
+	}
+	
+	@ResponseBody
+	@PostMapping("/user/register") //회원가입
 	public UserDto registerUser(@RequestBody UserDto userDto) {		
 		userService.insertUser(userDto);
 		
 		return userDto;
 	}
 	
+	@ResponseBody
 	@PostMapping("/myPage/change") 
 	public UserDto changeUser(@AuthenticationPrincipal UserDetails userDetail, @RequestBody UserDto userDto) {
 		UserEntity user = userRepository.findByEmail(userDetail.getUsername()); 
---
+++ itemrier_boot/src/main/webapp/WEB-INF/jsp/header.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<header id="header">
+        <h1>Itemrior</h1>
+        <nav class="item">
+            <ul>
+                <li><a href="">중고거래</a></li>
+                <li><a href="">공동구매</a></li>
+            </ul>
+        </nav>
+        <nav class="log">
+            <ul>
+                <li><sec:authorize access="isAnonymous()"><a href="/user/login/form">로그인</a></sec:authorize>
+               <sec:authorize access="isAuthenticated()"><a href="/logout">로그아웃</a></sec:authorize>
+				</li>      
+                <li><a href="/user/register/form">회원가입</a></li>
+            </ul>
+        </nav>
+    </header>
