4c3a6fb780a1dec653e70e7599ecad8529b86564
minsun <pitapatsun@gmail.com>
2024-06-19 23:43:17 +0900

    feat: 마이페이지 경매 판매중, 판매완료 백엔드 구현

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/service/myPage/AuctionHistoryService.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/service/myPage/AuctionHistoryService.java
@@ -1,5 +1,66 @@
 package com.hotSix.itemrier_boot.service.myPage;
 
-public class BidHistoryService {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.hotSix.itemrier_boot.domain.item.Auction;
+import com.hotSix.itemrier_boot.domain.item.ItemStatus;
+import com.hotSix.itemrier_boot.domain.item.UsedGoods;
+import com.hotSix.itemrier_boot.domain.user.UserEntity;
+import com.hotSix.itemrier_boot.dto.item.AuctionDto;
+import com.hotSix.itemrier_boot.dto.item.UsedGoodsDto;
+import com.hotSix.itemrier_boot.repository.item.AuctionRepository;
+import com.hotSix.itemrier_boot.repository.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class AuctionHistoryService {
+	private final AuctionRepository auctionRepository;
+	private final UserRepository userRepository;
+	
+	public List<AuctionDto> getAuctionsProgressSearch(int sellerId, Boolean status) {  // 공동 거래 판매 중 or 예약 중/판매완료된 내역 조회
+		List<Auction> auctions;
+
+		UserEntity seller = userRepository.findByUserId(sellerId);
+		
+		auctions = auctionRepository.findBySellerAndStatus(seller, ItemStatus.Available);
+		
+        List<AuctionDto> auctionDtoList = new ArrayList<>();
+        for (Auction auction : auctions) {
+        	AuctionDto auctionDto = auction.toDTO(auction);
+        	auctionDtoList.add(auctionDto);
+        }
+        System.out.println("판매중" + auctionDtoList);
+        return auctionDtoList;
+	}
+	
+	public HashMap<AuctionDto, String> getAuctionsEndedSearch(int sellerId, Boolean status) {  // 공동 거래 판매 중 or 예약 중/판매완료된 내역 조회
+		List<Auction> auctions;
+		HashMap<AuctionDto, String> auctionList = new HashMap<AuctionDto, String>();
+		UserEntity seller = userRepository.findByUserId(sellerId);
+
+		auctions = auctionRepository.findBySellerAndStatus(seller, ItemStatus.Complete);
+		
+        List<AuctionDto> auctionDtoList = new ArrayList<>();
+        for (Auction auction : auctions) {
+        	AuctionDto auctionDto = auction.toDTO(auction);
+        	auctionDtoList.add(auctionDto);
+        }
+        System.out.println("판매완료" + auctionDtoList);
+        
+		for (AuctionDto auction : auctionDtoList) {
+			if (auction.getWinner() == null) {
+				throw new NullPointerException();
+			}
+			UserEntity winner = userRepository.findByUserId(auction.getWinner());
+			auctionList.put(auction, winner.getNickname());
+		}
+        
+        return auctionList;
+	}
+	
 }
