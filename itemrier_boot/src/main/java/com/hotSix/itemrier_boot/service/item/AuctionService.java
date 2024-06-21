4e3075ab5bbf06464b1bbc2730724b2053af83ba
suhyun <sunny3872@naver.com>
2024-06-21 15:36:53 +0900

    feat 경매 search, recommend

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/service/item/AuctionService.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/service/item/AuctionService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -14,9 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.hotSix.itemrier_boot.domain.category.Category;
 import com.hotSix.itemrier_boot.domain.item.Auction;
+import com.hotSix.itemrier_boot.domain.item.GroupPurchase;
 import com.hotSix.itemrier_boot.domain.item.ItemStatus;
 import com.hotSix.itemrier_boot.domain.user.UserEntity;
 import com.hotSix.itemrier_boot.dto.item.AuctionDto;
+import com.hotSix.itemrier_boot.dto.item.GroupPurchaseDto;
 import com.hotSix.itemrier_boot.repository.item.AuctionRepository;
 import com.hotSix.itemrier_boot.repository.item.CategoryRepository;
 import com.hotSix.itemrier_boot.repository.user.UserRepository;
@@ -27,40 +30,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuctionService {	
 	private final AuctionRepository auctionRepository;
-	
+
 	private final UserRepository userRepository;
-	
+
 	private final CategoryRepository categoryRepoistory;
-	
+
 	// 경매 상품 등록
 	public void save(AuctionDto dto, int sellerId, MultipartFile file) throws Exception {
 		UserEntity seller = userRepository.findByUserId(sellerId);
 		dto.setSeller(seller);
-		
+
 		Category category = categoryRepoistory.getReferenceById(Integer.parseInt(dto.getCatId()));
 		dto.setCategory(category);
-		
+
 		dto.setStatus(ItemStatus.Available);
 		dto.setCurrentBid(dto.getStartPrice());
-		
+
 		// 파일 처리
 		String fileName = saveFile(file);
 		dto.setFileName(fileName);
 		dto.setFilePath("/files/" + fileName);
-		
+
 		Auction auction = dto.toEntity(dto);
 		auctionRepository.save(auction);
 	}
-	
+
 	// 경매 상품 수정
 	public AuctionDto update(AuctionDto dto) {
 		Auction originalAuction = auctionRepository.getReferenceById(dto.getItemId());
 		originalAuction.update(dto);
-		
+
 		auctionRepository.save(originalAuction);
 		return findById(dto.getItemId());
 	}
-	
+
 	// 경매 상품 삭제
 	public void delete(int itemId) throws Exception {
 		Auction auction = auctionRepository.getReferenceById(itemId);
@@ -70,12 +73,12 @@ public class AuctionService {
 		}
 		auctionRepository.deleteById(itemId);
 	}
-	
+
 	// 경매 상품 전체 목록 보기
 	public List<AuctionDto> findAll() {
 		List<Auction> auctionList = auctionRepository.findAllByOrderByStartTimeDesc();
 		List<AuctionDto> auctionDtoList = new ArrayList<>();
-		
+
 		for (Auction auction: auctionList) {
 			AuctionDto auctionDto = new AuctionDto();
 			auctionDto.setItemId(auction.getItemId());
@@ -88,14 +91,14 @@ public class AuctionService {
 		}
 		return auctionDtoList;
 	}
-	
+
 	// 경매 상품 상세보기
 	public AuctionDto findById(int itemId) {
 		Auction auction = auctionRepository.getReferenceById(itemId);
 		AuctionDto auctionDto = AuctionDto.toDTO(auction);
 		return auctionDto;
 	}
-	
+
 	// 유효성 체크
 	public Map<String, String> validateHandling(BindingResult result) {
 		Map<String, String> validatorResult = new HashMap<>();
@@ -105,7 +108,7 @@ public class AuctionService {
 		}
 		return validatorResult;
 	}
-	
+
 	// 파일 처리
 	public String saveFile(MultipartFile file) throws Exception {
 		String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/files/";
@@ -115,15 +118,31 @@ public class AuctionService {
 		file.transferTo(saveFile);
 		return fileName;
 	}
-	
+
 	// 파일 삭제
 	public void deleteFile(String fileName) throws Exception {
-        String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/files/";
-        File file = new File(projectPath, fileName);
-        if (file.exists()) {
-        	if (!file.delete()) {
-                throw new Exception("Failed to delete file: " + fileName);
-            } 
-        }
+		String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/files/";
+		File file = new File(projectPath, fileName);
+		if (file.exists()) {
+			if (!file.delete()) {
+				throw new Exception("Failed to delete file: " + fileName);
+			} 
+		}
+	}
+
+	// 검색
+	public List<AuctionDto> searchAuction(String query) {
+        List<Auction> searchResults = auctionRepository.findByItemNameContaining(query);
+        return searchResults.stream()
+				.map(auction -> auction.toDTO(auction))
+                            .collect(Collectors.toList());
+    }
+
+	//	추천
+	public List<AuctionDto> recommend(int categoryId, int itemId) {
+		List<Auction> recommendedItems = auctionRepository.findTop5ByCategoryCatIdAndItemIdNotOrderByStartTimeDesc(categoryId, itemId);
+		return recommendedItems.stream()
+				.map(auction -> auction.toDTO(auction))
+				.collect(Collectors.toList());
 	}
 }
