3bacc51d4b84382d7b5ffa4a8dbcb4f69166d7cd
minsun <pitapatsun@gmail.com>
2024-09-13 13:52:31 +0900

    refactor : 불필요한 코드 삭제

--- itemrier_boot/src/main/java/com/hotSix/itemrier_boot/sercurity/handler/CustomAuthenticationSuccessHandler.java
+++ itemrier_boot/src/main/java/com/hotSix/itemrier_boot/sercurity/handler/CustomAuthenticationSuccessHandler.java
@@ -1,45 +1,46 @@
-package com.hotSix.itemrier_boot.sercurity.handler;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.WebAttributes;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
-
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
-        emailLoginSuccess(response);
-    }
-
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        emailLoginSuccess(response);
-    }
-
-    // 로그인 실패 후 성공 시 남아있는 에러 세션 제거
-    protected void clearSession(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-        }
-    }
-    
-    public void emailLoginSuccess(HttpServletResponse response) throws IOException {
-        String jsonResponse = new ObjectMapper().writeValueAsString("성공");
-
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(jsonResponse);
-        response.sendRedirect("/");
-    }
-}
+//package com.hotSix.itemrier_boot.sercurity.handler;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.WebAttributes;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//
+//import java.io.IOException;
+//
+//public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+//        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+//        emailLoginSuccess(response);
+//    }
+//
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//        emailLoginSuccess(response);
+//    }
+//
+//    // 로그인 실패 후 성공 시 남아있는 에러 세션 제거
+//    protected void clearSession(HttpServletRequest request) {
+//        HttpSession session = request.getSession(false);
+//        if (session != null) {
+//            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+//        }
+//    }
+//    
+//    //사용 x
+//    public void emailLoginSuccess(HttpServletResponse response) throws IOException {
+//        String jsonResponse = new ObjectMapper().writeValueAsString("성공");
+//
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        response.setContentType("application/json");
+//        response.setCharacterEncoding("UTF-8");
+//        response.getWriter().write(jsonResponse);
+//        response.sendRedirect("/");
+//    }
+//}
